e4ea9e6382be5116816bf31d36771cb050501b44
akshaiim <mishraakshaiii@gmail.com>
2022-06-15 12:17:46 +0530

    mint-nft

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+
+require('@nomiclabs/hardhat-waffle');
+require("dotenv").config({ path: ".env" });
+console.log(process.env.ALCHEMY_API_KEY_URL)
+
+module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
